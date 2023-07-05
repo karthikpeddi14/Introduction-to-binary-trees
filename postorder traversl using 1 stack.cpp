5ad738e84ee13705ff5d147f539055ef53ff5e4a
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-07-05 13:53:53 +0530

    Create postorder traversl using 1 stack.cpp

---
+++ postorder traversl using 1 stack.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) 
+    {   
+        if(root == NULL) return {} ;  
+        stack<TreeNode*> st;
+        vector<int> ans;
+        TreeNode* cur = root;
+        TreeNode* temp;
+
+        while(cur!=NULL || !st.empty())
+        {
+            if(cur!=NULL)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                temp = st.top()->right;
+                if(temp!=NULL) cur = temp;
+                else
+                {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right )
+                    {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+            }
+        } 
+        return ans;  
+    }
+};
